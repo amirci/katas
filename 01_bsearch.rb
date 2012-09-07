5ee9531ffad4aac2367b195f4ee3da7a393bc1e3
Matt <mzandstra@childcarepro.ca>
2012-09-07 11:20:46 -0500

    kata1 is binary search, solved using recursion

---
+++ 01_bsearch.rb
@@ -0,0 +1,67 @@
+require 'test/unit'
+
+class TestChop < Test::Unit::TestCase
+	
+	def chop(n, h)
+		#iterative_chop n, h
+		recursive_chop n, h
+	end
+	
+	def iterative_chop(n, h)
+		#broken
+		begin
+			low = 0
+			high = h.size
+			mid = low+(high-low/2)-1
+			
+			if h.size==0 || h.size == 1 && h[mid] != n
+				-1
+			elsif h[mid] > n
+				h.slice(low,mid)
+			elsif h[mid] < n
+				h.slice(mid,high)
+			end
+		end while low >= high
+	end
+	
+	def recursive_chop (n, h)
+		low = 0
+		high = h.size
+		mid = low+(high-low/2)-1
+		#puts "Looking for #{n} in #{h.inspect} (low/mid/high: #{low}/#{mid}/#{high})"
+		
+		if h.size==0 || h.size==1 && h[mid] != n
+			-1
+		elsif h[mid] > n
+			chop n, h[low,mid]
+		elsif h[mid] < n
+			chop n, h[mid,high]
+		else
+			mid
+		end
+	end
+	
+	def test_chop
+			assert_equal(-1, chop(3, []))
+			assert_equal(-1, chop(3, [1]))
+			assert_equal(0,  chop(1, [1]))
+			
+			assert_equal(0,  chop(1, [1, 3, 5]))
+			assert_equal(1,  chop(3, [1, 3, 5]))
+			assert_equal(2,  chop(5, [1, 3, 5]))
+			assert_equal(-1, chop(0, [1, 3, 5]))
+			assert_equal(-1, chop(2, [1, 3, 5]))
+			assert_equal(-1, chop(4, [1, 3, 5]))
+			assert_equal(-1, chop(6, [1, 3, 5]))
+			
+			assert_equal(0,  chop(1, [1, 3, 5, 7]))
+			assert_equal(1,  chop(3, [1, 3, 5, 7]))
+			assert_equal(2,  chop(5, [1, 3, 5, 7]))
+			assert_equal(3,  chop(7, [1, 3, 5, 7]))
+			assert_equal(-1, chop(0, [1, 3, 5, 7]))
+			assert_equal(-1, chop(2, [1, 3, 5, 7]))
+			assert_equal(-1, chop(4, [1, 3, 5, 7]))
+			assert_equal(-1, chop(6, [1, 3, 5, 7]))
+			assert_equal(-1, chop(8, [1, 3, 5, 7]))
+	end
+end
